36c2d0f2010f6abb3fb0bc610639b31598aa7f98
daijir <dazhilangxianggen@gmail.com>
2025-05-15 16:59:33 +0900

    finished assignment 2

---
+++ database/assignment2.sql
@@ -0,0 +1,40 @@
+-- Task 1
+-- 1. Insert
+-- INSERT INTO public.account (
+--     account_firstname,
+--     account_lastname,
+--     account_email,
+--     account_password
+-- ) VALUES (
+--     'Tony',
+--     'Stark',
+--     'tony@starkent.com',
+--     'Iam1ronM@n'
+-- );
+
+-- -- 2. Modify
+-- UPDATE public.account
+-- SET account_type = 'Admin'
+-- WHERE account_id = 1;
+
+-- -- 3. Delete
+-- DELETE FROM public.account
+-- WHERE account_id = 1;
+
+-- -- 5. Inner join
+-- SELECT i.inv_make, i.inv_model, c.classification_name
+-- FROM public.inventory i
+-- INNER JOIN public.classification c
+--   ON i.classification_id = c.classification_id
+-- WHERE c.classification_name = 'Sport';
+
+-- 4. Modify the "GM Hummer" record to read "a huge interior"
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+-- 6. update all records in the inventory table...
+UPDATE public.inventory
+SET
+  inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+  inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
