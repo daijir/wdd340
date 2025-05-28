81505934139006ed4c3e527d707dc7a3c1412ac2
daijir <dazhilangxianggen@gmail.com>
2025-05-28 21:50:32 +0900

    finished activity

---
+++ routes/accountRoute.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = new express.Router();
+const utilities = require("../utilities");
+const accountController = require("../controllers/accountController");
+const regValidate = require('../utilities/account-validation');
+
+/* ****************************************
+*  Deliver login view
+* *************************************** */
+router.get("/login", utilities.handleErrors(accountController.buildLogin))
+
+/* ****************************************
+*  Deliver registration view
+* *************************************** */
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
+/* ****************************************
+*  register a new account
+* *************************************** */
+router.post(
+  '/register',
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
+
+module.exports = router;
